2f0bfbf01e75cb52b9647e019a72d643b866ec2d
RadicalZulu110 <49160918+RadicalZulu110@users.noreply.github.com>
2020-02-29 18:25:18 +0000

    AI seeking added

---
+++ Game/Assets/Mike/Scripts/Guard.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UI;
+
+
+public class Guard : MonoBehaviour
+{
+
+    public Transform Player;
+
+    int MoveSpeed = 20;
+    int MaxDist = 10;
+    int MinDist = 2;
+
+    private NavMeshAgent navmesh;
+
+    void Start()
+    {
+        navmesh = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        transform.LookAt(Player);
+
+        if (Vector3.Distance(transform.position, Player.position) >= MinDist)
+        {
+
+            transform.position += transform.forward * MoveSpeed * Time.deltaTime;
+
+
+
+            if (Vector3.Distance(transform.position, Player.position) <= MaxDist)
+            {
+                //Here Call any function U want Like Shoot at here or something
+            }
+
+        }
+    }
+}
