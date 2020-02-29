c6322e2eb85beb35a0057a852824eb19b378de34
VeryParadoxical <34679234+VeryParadoxical@users.noreply.github.com>
2020-02-29 16:23:48 +0000

    AI path towards player with trigger
    Currently a tad buggy but good progress is being made. Use AITestScene to test this functionality.

---
+++ Game/Assets/Enemy_AI/AI_EnemyCS.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AI_EnemyCS : MonoBehaviour
+{
+    public GameObject player;
+
+    private NavMeshAgent navmesh;
+
+    void Start()
+    {
+        navmesh = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        navmesh.destination = player.transform.position;
+    }
+}
+
