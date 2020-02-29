f6bbcce25a2a7701d0d5af73a2ffecfbec9e5f05
Sam Herod <samherod18@gmail.com>
2020-02-29 20:09:31 +0000

    TimedSpawners

---
+++ Game/Assets/Scripts/timedSpawner.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class timedSpawner : MonoBehaviour
+{
+
+    public GameObject spawnedEnemy;
+    public bool       stopSpawning = false;
+    public float      spawnTime;
+    public float      spawnDelay;
+    public int        spawnMax;
+           int        count;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnObject", spawnTime, spawnDelay);
+    }
+
+    public void SpawnObject()
+    {
+        if (count >= spawnMax)
+        {
+            CancelInvoke("SpawnObject");
+        }
+        else
+        {
+            Instantiate(spawnedEnemy, transform.position, transform.rotation);
+        }
+        count++;
+    }
+}
